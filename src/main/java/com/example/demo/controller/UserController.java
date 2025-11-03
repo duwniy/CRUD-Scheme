0e8bd13ba8f15feca1bc21d5031ef37e48fa3024
duwniy <execmyaccaunt@gmail.com>
2025-11-03 11:29:28 +0500

    fixes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDTO;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -20,44 +18,32 @@ public class UserController {
     }
 
     @GetMapping
-    public List<UserDTO> getUsers() {
-        return service.getAllUsers().stream()
-                .map(user -> new UserDTO(user.getName(), user.getEmail()))
-                .collect(Collectors.toList());
+    public List<User> getUsers() {
+        return service.getAllUsers();
     }
 
     @GetMapping("/{email}")
-    public UserDTO getUser(@PathVariable String email) {
-        User user = service.getUserByEmail(email);
-        return new UserDTO(user.getName(), user.getEmail());
+    public User getUser(@PathVariable String email) {
+        return service.getUserByEmail(email);
     }
 
     @PostMapping
-    public UserDTO addUser(@RequestBody UserDTO userDTO) {
-        User created = service.createUser(new User(userDTO.getName(), userDTO.getEmail()));
-        return new UserDTO(created.getName(), created.getEmail());
+    public User addUser(@RequestBody User user) {
+        return service.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return service.updateUser(id, user);
+    }
+
+    @PatchMapping("/{id}")
+    public User patchUser(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        return service.patchUser(id, updates);
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         service.deleteUser(id);
-    }
-
-    @PutMapping("/{id}")
-    public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
-        User updated = new User(userDTO.getName(), userDTO.getEmail());
-        User result = service.updateUser(id, updated);
-        return new UserDTO(result.getName(), result.getEmail());
-    }
-
-    @PatchMapping("/{id}")
-    public UserDTO patchUser(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
-        User patched = service.patchUser(id, updates);
-        return new UserDTO(patched.getName(), patched.getEmail());
-    }
-
-    @GetMapping("/button")
-    public String button() {
-        return "redirect:/button.html";
     }
 }
